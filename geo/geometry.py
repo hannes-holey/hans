cd36dd032ff14f725aa2a53200df0b97c52f217b
Hannes Holey <hannes.holey@kit.edu>
2020-02-27 08:46:41 +0100

    geometry input in separate class

---
+++ geo/geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+class Analytic:
+
+    def __init__(self, geometry):
+
+        self.geometry = geometry
+        self.Lx = float(geometry['Lx'])
+        self.Ly = float(geometry['Ly'])
+        self.h1 = float(geometry['h1'])
+        self.h2 = float(geometry['h2'])
+
+
+
+    def linearSlider(self, x , y):
+        "Linear height profile"
+        sx = (self.h2 - self.h1)/(self.Lx)
+        sy = 0.
+        return self.h1 + sx * x + sy * y
+
+    def journalBearing(self, x, y):
+
+        ex = float(self.geometry['ex'])
+
+        R1 = self.Lx/(2*np.pi)
+        c = 1.e-2 * R1
+        R0 = R1 + c
+        e = ex * c
+
+        return c + e * np.cos(2. * np.pi * x / self.Lx)
