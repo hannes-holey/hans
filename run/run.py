b5770daf1e8a63cf0da977350ea09165ac1242e9
Hannes Holey <hannes.holey@kit.edu>
2020-03-04 10:14:46 +0100

    ENH: Progress bar in silent mode (#9)

--- run/run.py
+++ run/run.py
@@ -40,8 +40,10 @@ class Run:
         self.sol = Solver(options, disc, geometry, numerics, material)
 
         if self.plotOption == 0:
+            self.printProgressBar(0, self.maxIt)
             for i in range(self.maxIt):
                 self.sol.solve(i)
+                self.printProgressBar(i + 1, self.maxIt)
         else:
             self.plot()
 
@@ -211,3 +213,24 @@ class Run:
         sol.solve(i)
 
         return stream
+
+    def printProgressBar(self, iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+            printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+        # Print New Line on Complete
+        if iteration == total:
+            print()
