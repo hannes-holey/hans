f3e5b5c70a8ce8c1a0c440aa6623c2ee21814fbd
Hannes Holey <hannes.holey@kit.edu>
2020-03-09 15:43:48 +0100

    ENH: add thermodynamic speed of sound computation

--- eos/eos.py
+++ eos/eos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 class DowsonHigginson:
 
     def __init__(self, material):
@@ -10,11 +12,18 @@ class DowsonHigginson:
     def isoT_pressure(self, rho):
         B1 = 6.6009e-10
         B2 = 2.8225e-9
-        P0= 101325.
-        return (rho - self.rho0)/(B1*self.rho0 - B2*(rho - self.rho0)) + P0
+
+        return (rho - self.rho0)/(B1*self.rho0 - B2*(rho - self.rho0)) + self.P0
 
     def isoT_density(self, P):
         B1 = 6.6009e-10
         B2 = 2.8225e-9
-        P0= 101325.
-        return self.rho0 * (1. + B1*(P - P0) /(1.+B2*(P-P0)))
+
+        return self.rho0 * (1. + B1*(P - self.P0) /(1.+B2*(P - self.P0)))
+
+    def soundSpeed(self, rho):
+        B1 = 6.6009e-10
+        B2 = 2.8225e-9
+
+        return np.sqrt(B1*self.rho0/((B1 + B2)*self.rho0 - B2*rho)**2)
+        #return -1./rho * np.sqrt(-B1*self.rho0/((self.rho0/rho * (B1 + B2) - B2)**2))
