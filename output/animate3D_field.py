66f9065b9b5cf6020b4906539f35c471b5ab8491
Hannes Holey <hannes.holey@kit.edu>
2020-03-19 14:50:02 +0100

    ENH: 3D animation post-processing script rewritten

--- output/animate3D_field.py
+++ output/animate3D_field.py
@@ -1,75 +1,117 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.colors import LightSource
-from matplotlib import cm
+import matplotlib.animation as animation
 
-plt.style.use('presentation')
-fig = plt.figure(figsize=(8,6))
+def assembleArrays(filename, database):
+    """
+    Assemble list of arrays from hdf5-file to loop over in animation
 
-file = h5py.File(sys.argv[1], 'r')
+    Parameters
+    ----------
+    filename :  str
+                name of the hdf5 input file
+    database :  str
+                name of the database to read from
 
-conf_opt =  file.get('/config/options')
-conf_num =  file.get('config/numerics')
-conf_disc = file.get('config/disc')
+    Returns
+    -------
+    A :  list
+         list of result arrays
+    t :  list
+         list of snapshot times
+    Nx : int
+         number of grid cells in x
+    Ny : int
+         number of grid cells in y
+    """
+    file = h5py.File(filename, 'r')
+    conf_disc = file.get('/config/disc')
+    Nx = conf_disc.attrs['Nx']
+    Ny = conf_disc.attrs['Ny']
+    Lx = conf_disc.attrs['Lx']
+    Ly = conf_disc.attrs['Ly']
 
-Nx = conf_disc.attrs['Nx']
-Ny = conf_disc.attrs['Ny']
-Lx = conf_disc.attrs['Lx']
-Ly = conf_disc.attrs['Ly']
+    A = []
+    t = []
+    for i in file.keys():
+        if str(i) != 'config':
+            g = file.get(i)
+            A.append(np.array(g.get(database)))
+            t.append(g.attrs['time']*1e9)
+    file.close()
 
-x = np.linspace(0, Lx, Nx)
-y = np.linspace(0, Ly, Ny)
+    return A, t, Nx, Ny, Lx, Ly
 
-xx, yy = np.meshgrid(x,y)
+def plot_update(i, A, t):
+    """
+    Updates the plot in animation
 
-toPlot = {0:'j_x', 1:'j_y', 2:'rho', 3:'press'}
-choice = int(input("What to plot? (0: maxx flux x | 1: mass flux y | 2: density | 3: pressure) "))
+    Parameters
+    ----------
+    i : int
+        iterator
+    A : list
+        list of result arrays
+    t : list
+        list of snapshot times
+    """
+    ax.cla()
 
+    global glob_min
+    global glob_max
 
-# im = ax.imshow(np.empty((Nx,Ny)), interpolation='nearest', cmap='viridis')
-# cbar = plt.colorbar(im, ax = ax)
+    if np.amin(A[i]) < glob_min:
+        glob_min = np.amin(A[i])
+    if np.amax(A[i]) > glob_max:
+        glob_max = np.amax(A[i])
 
-ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(xx,yy, A[i].T, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='viridis')
+    ax.grid(False)
+    ax.set_zlim(glob_min, glob_max)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    fig.suptitle("Time: {:.1f}".format(t[i]))
 
-light = LightSource(90, 45)
+if __name__ == "__main__":
 
+    plt.style.use('presentation')
+    fig = plt.figure(figsize=(8,6))
+    ax = Axes3D(fig)
 
-for i in file.keys():
-    if str(i) != 'config':
+    filename = sys.argv[1]
+    name = os.path.splitext(filename)[0]
 
-        g = file.get(i)
-        d = np.array(g.get(toPlot[choice]))
+    # User input
+    toPlot = {0:'j_x', 1:'j_y', 2:'rho', 3:'press'}
+    choice = int(input("What to plot? (0: maxx flux x | 1: mass flux y | 2: density | 3: pressure) "))
+    save = int(input("Show (0) or save (1) animation? "))
 
-        t = g.attrs['time']*1e9
-        ax.clear()
-        # illuminated_surface = light.shade(d, cmap=cm.coolwarm)
-        ax.plot_surface(xx,yy,d, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='viridis')
+    A, t, Nx, Ny, Lx, Ly = assembleArrays(filename, toPlot[choice])
 
-        # im.set_array(d)
-        # im.set_clim(vmin=np.amin(d),vmax=np.amax(d))
+    # Global colorbar limits
+    glob_min = np.amin(A[0])
+    glob_max = np.amax(A[0])
 
-    plt.pause(0.0001)
+    # Initial plotting
+    x = np.linspace(0, Lx, Nx)
+    y = np.linspace(0, Ly, Ny)
+    xx, yy = np.meshgrid(x,y)
+    ax.plot_surface(xx,yy, A[0].T, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap='viridis')
+    ax.grid(False)
+    ax.set_zlim(glob_min, glob_max)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
-# plt.show()
-file.close()
+    # Create animation
+    anim = animation.FuncAnimation(fig, plot_update, frames=len(A), fargs=(A, t,), interval=500, repeat=True)
 
-# plt.title()
-
-# if choice == 0:
-#     ylab =  'mass flux x'
-# elif  choice == 1:
-#     ylab =  'mass flux y'
-# elif  choice == 2:
-#     ylab =  'density'
-# else:
-#     ylab = 'pressure (Pa)'
-#
-# plt.xlabel('distance (mm)')
-# plt.ylabel(ylab)
-
-#plt.legend(loc = 'best')
+    if save == 1:
+        anim.save(name + '_' + toPlot[choice] + '_3D.mp4' , fps=30)
+    elif save == 0:
+        plt.show()
