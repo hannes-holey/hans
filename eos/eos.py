9e07a546c28d676fba66ae6fe8f715ba48205258
Hannes Holey <hannes.holey@kit.edu>
2020-03-27 09:52:41 +0100

    MAINT: change form of Dowson-Higginson parameter

--- eos/eos.py
+++ eos/eos.py
@@ -8,17 +8,30 @@ class DowsonHigginson:
     def __init__(self, material):
         self.rho0 = float(material['rho0'])
         self.P0 = float(material['P0'])
-        self.B1 = float(material['B1'])
-        self.B2 = float(material['B2'])
+
+        self.C1 = float(material['C1'])
+        self.C2 = float(material['C2'])
+
+        # self.B1 = float(material['B1'])
+        # self.B2 = float(material['B2'])
 
     def isoT_pressure(self, rho):
-        return (rho - self.rho0)/(self.B1*self.rho0 - self.B2*(rho - self.rho0)) + self.P0
+        return self.P0 + (self.C1 * (rho/self.rho0 - 1.))/(self.C2 - rho/self.rho0)
 
     def isoT_density(self, P):
-        return self.rho0 * (1. + self.B1*(P - self.P0) /(1. + self.B2*(P - self.P0)))
+        return self.rho0 * (self.C1 + self.C2 * (P - self.P0))/(self.C1 + P - self.P0)
 
     def soundSpeed(self, rho):
-        return np.sqrt(self.B1*self.rho0/((self.B1 + self.B2)*self.rho0 - self.B2 * rho)**2)
+        return np.sqrt(self.C1 * self.rho0 *(self.C2 -1.)*(1/rho)**2 /((self.C2 * self.rho0/rho -1.)**2))
+
+    # def isoT_pressure(self, rho):
+    #     return (rho - self.rho0)/(self.B1*self.rho0 - self.B2*(rho - self.rho0)) + self.P0
+    #
+    # def isoT_density(self, P):
+    #     return self.rho0 * (1. + self.B1*(P - self.P0) /(1. + self.B2*(P - self.P0)))
+    #
+    # def soundSpeed(self, rho):
+    #     return np.sqrt(self.B1*self.rho0/((self.B1 + self.B2)*self.rho0 - self.B2 * rho)**2)
 
 class PowerLaw:
     def __init__(self, material):
