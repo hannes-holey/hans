44814c56f77ec93d5f3d891b7c762b4e3871f508
Hannes Holey <hannes.holey@kit.edu>
2020-04-08 09:20:42 +0200

    MAINT: remove unnecessary stuff

--- eos/eos.py
+++ eos/eos.py
@@ -8,12 +8,8 @@ class DowsonHigginson:
     def __init__(self, material):
         self.rho0 = float(material['rho0'])
         self.P0 = float(material['P0'])
-
         self.C1 = float(material['C1'])
         self.C2 = float(material['C2'])
-
-        # self.B1 = float(material['B1'])
-        # self.B2 = float(material['B2'])
 
     def isoT_pressure(self, rho):
         return self.P0 + (self.C1 * (rho/self.rho0 - 1.))/(self.C2 - rho/self.rho0)
@@ -23,15 +19,6 @@ class DowsonHigginson:
 
     def soundSpeed(self, rho):
         return np.sqrt(self.C1 * self.rho0 *(self.C2 -1.)*(1/rho)**2 /((self.C2 * self.rho0/rho -1.)**2))
-
-    # def isoT_pressure(self, rho):
-    #     return (rho - self.rho0)/(self.B1*self.rho0 - self.B2*(rho - self.rho0)) + self.P0
-    #
-    # def isoT_density(self, P):
-    #     return self.rho0 * (1. + self.B1*(P - self.P0) /(1. + self.B2*(P - self.P0)))
-    #
-    # def soundSpeed(self, rho):
-    #     return np.sqrt(self.B1*self.rho0/((self.B1 + self.B2)*self.rho0 - self.B2 * rho)**2)
 
 class PowerLaw:
     def __init__(self, material):
