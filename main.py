a772117763e73187933639a3fad27f820f91c08f
Hannes Holey <hannes.holey@kit.edu>
2020-04-28 17:50:04 +0200

    DOC: add docstrings

--- main.py
+++ main.py
@@ -7,12 +7,34 @@ import argparse
 
 
 class yamlInput:
+    """Reads the yaml input file to define a problem.
+
+    Attributes
+    ----------
+    inputFile : str
+        filename of the .yaml input file
+    """
 
     def __init__(self, inputFile):
-        self.inputFile = inputFile
-        self.read()
+        """ Constructor
 
-    def read(self):
+        Parameters
+        ----------
+        inputFile : str
+            filename of the .yaml input file
+        """
+        self.inputFile = inputFile
+
+    def getProblem(self):
+        """Parses through yaml file and returns an instance of the Problem class.
+
+        Returns
+        -------
+        Problem
+            Object containing all information about the current job.
+
+        """
+
         with open(self.inputFile, 'r') as ymlfile:
             inp = yaml.full_load(ymlfile)
 
@@ -22,16 +44,45 @@ class yamlInput:
             self.numerics = inp['numerics']
             self.material = inp['material']
 
-    def getProblem(self):
-
         thisProblem = Problem(self.options, self.disc, self.geometry, self.numerics, self.material)
 
         return thisProblem
 
 
 class Problem:
+    """Problem class, contains all information about a specific simulation
+
+    Attributes
+    ----------
+    options : dict
+        Contains IO options.
+    disc : dict
+        Contains discretization parameters.
+    geometry : dict
+        Contains geometry parameters.
+    numerics : dict
+        Contains parameters for the numeric scheme.
+    material : dict
+        Contains material parameters.
+
+    """
 
     def __init__(self, options, disc, geometry, numerics, material):
+        """Constructor
+
+        Parameters
+        ----------
+        options : dict
+            Contains IO options.
+        disc : dict
+            Contains discretization parameters.
+        geometry : dict
+            Contains geometry parameters.
+        numerics : dict
+            Contains parameters for the numeric scheme.
+        material : dict
+            Contains material parameters.
+        """
 
         self.options = options
         self.disc = disc
