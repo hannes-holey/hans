ab5b8c13d08ee33f2bd6de655c23b762d9c0f879
Hannes Holey <hannes.holey@kit.edu>
2020-05-05 12:34:30 +0200

    WIP: do not open temporary copy of file, but file itself

--- output/helper.py
+++ output/helper.py
@@ -32,11 +32,12 @@ def getFiles():
     ask = True
     j = 0
     while ask is True:
-        userInput = input("Enter file key (any other key to exit): ")
-        if userInput in np.arange(0, len(availFiles)).astype(str):
-            tmp_file = copyTemp(availFiles[int(userInput)][0])
-            files.update({j: [availFiles[int(userInput)][0], h5py.File(tmp_file, 'r')]})
-            subprocess.call("rm " + tmp_file, shell=True)
+        userInput = int(input("Enter file key (any other key to exit): "))
+        if userInput in np.arange(0, len(availFiles)):
+            # tmp_file = copyTemp(availFiles[int(userInput)][0])
+            # files.update({j: [availFiles[int(userInput)][0], h5py.File(tmp_file, 'r')]})
+            # subprocess.call("rm " + tmp_file, shell=True)
+            files.update({j: [availFiles[userInput][0], h5py.File(availFiles[userInput][0], 'r')]})
             j += 1
         else:
             ask = False
@@ -67,10 +68,10 @@ def getFile():
 
     flag = False
     while flag is False:
-        userInput = input("Enter file key: ")
-        if userInput in np.arange(0, len(availFiles)).astype(str):
+        userInput = int(input("Enter file key: "))
+        if userInput in np.arange(0, len(availFiles)):
             # tmp_file = copyTemp(availFiles[int(userInput)][0])
-            file = h5py.File(availFiles[int(userInput)][0], 'r')
+            file = h5py.File(availFiles[userInput][0], 'r')
             # file = h5py.File(tmp_file, 'r')
             # subprocess.call("rm " + tmp_file, shell=True)
             flag = True
@@ -97,9 +98,9 @@ def getReference():
     for key, val in availFiles.items():
         print("{:3d}: {:20s}".format(key, val))
 
-    userInput = input("Enter file key: ")
-    if userInput in np.arange(0, len(availFiles)).astype(str):
-        ref = np.loadtxt(availFiles[int(userInput)], skiprows=1)
+    userInput = int(input("Enter file key: "))
+    if userInput in np.arange(0, len(availFiles)):
+        ref = np.loadtxt(availFiles[userInput], skiprows=1)
         assert ref.ndim == 2, "Input data file has wrong dimension"
         return ref
     else:
