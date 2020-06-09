6d70f5adf268ca72b69cd1f40ecddd4a99547331
Hannes Holey <hannes.holey@kit.edu>
2020-06-09 09:34:06 +0200

    ENH: get all variance/mean pairs at once

--- output/get_variance.py
+++ output/get_variance.py
@@ -3,10 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from helper import getFile
-from scipy.optimize import curve_fit
+# from scipy.optimize import curve_fit
 
 plt.style.use('presentation')
-fig, ax = plt.subplots(2,1, figsize=(12,16))
+fig, ax = plt.subplots(3,2, figsize=(12,16))
 
 
 def gaussian(x, a, mean, sigma):
@@ -23,51 +23,50 @@ toPlot = {0: ['jx', r'mass flux $x$ [kg/(m$^2$s)]', 1e1],       # last item in l
           2: ['rho', r'density [kg/m$^3$]', 1e3],
           3: ['p', r'pressure (MPa)', 1e-6]}
 
-reduced = not('p' in file.variables)
+choices = np.arange(3)
 
-if reduced is True:
-    choice = int(input("Choose field variable to plot:\n0:\tmass flux x\n1:\tmass flux y\n2:\tdensity\n"))
-else:
-    choice = int(input("Choose field variable to plot:\n0:\tmass flux x\n1:\tmass flux y\n2:\tdensity\n3:\tpressure\n"))
+# table header
+print("{:^10s}| {:^17s} | {:^17s} ".format("","mean","variance"))
+print(50 * '-')
 
-# check if enough memory available, load data
-try:
-    full_array = np.array(file.variables[toPlot[choice][0]], copy=False) / toPlot[choice][2]
-except MemoryError:
-    print("Not enough memory, using single precision!")
-    full_array = np.array(file.variables[toPlot[choice][0]], copy=False).astype(np.float32) / toPlot[choice][2]
+for choice in choices:
+    # check if enough memory available, load data
+    try:
+        full_array = np.array(file.variables[toPlot[choice][0]], copy=False) / toPlot[choice][2]
+    except MemoryError:
+        print("Not enough memory, using single precision!")
+        full_array = np.array(file.variables[toPlot[choice][0]], copy=False).astype(np.float32) / toPlot[choice][2]
 
-cellVariance = np.var(full_array, axis=0)
-variance = np.mean(cellVariance)
-mean = np.mean(full_array)
-print("mean : {:.10e}, variance : {:.10e}".format(mean, variance))
+    cellVariance = np.var(full_array, axis=0)
+    variance = np.mean(cellVariance)
+    mean = np.mean(full_array)
+
+    print("{:^10s}| {: .10e} | {: .10e} ".format(toPlot[choice][0], mean, variance))
 
 # --- Loop over n different sampling numbers --- #
-n = 10
-var = np.zeros((n,2))
-size = file.dimensions['step'].size
+    n = 10
+    var = np.zeros((n,2))
+    size = file.dimensions['step'].size
 
-for i in range(n):
-    samples = (i + 1) * size // n
+    for i in range(n):
+        samples = (i + 1) * size // n
 
-    cellVariance = np.var(full_array[0:samples + 1], axis=0)
-    variance = np.mean(cellVariance)
+        cellVariance = np.var(full_array[0:samples + 1], axis=0)
+        variance = np.mean(cellVariance)
 
-    var[i,0] = samples
-    var[i,1] = variance
+        var[i,0] = samples
+        var[i,1] = variance
 
-# np.savetxt('var_' + toPlot[choice][0] + '-vs-N.dat', var, header="samples variance(cgs units)", comments="")
-ax[0].plot(var[:,0], var[:,1], '-o')
+    col = 'C' + str(choice)
+    # np.savetxt('var_' + toPlot[choice][0] + '-vs-N.dat', var, header="samples variance(cgs units)", comments="")
+    ax[choice, 1].plot(var[:,0], var[:,1], '-o', color=col)
 
-# plot histogram of fluctuating quantity in the center cell
-yhist, xhist, _ = plt.hist(full_array[:,Nx // 2, Ny // 2], bins=25)
+    # plot histogram of mean value time series
+    mean_t = np.mean(full_array, axis=(1,2))
+    yhist, xhist, _ = ax[choice, 0].hist(mean_t, bins=25, color=col)
 
-bin_width = xhist[-1] - xhist[-2]
-
-# fit un-normalized Gaussian to the data and plot
-popt, pcov = curve_fit(gaussian, xhist[:-1] + bin_width / 2, yhist, [np.amax(xhist), mean, np.sqrt(variance)])
-x = np.linspace(xhist[0],xhist[-1], 100)
-ax[1].plot(x, gaussian(x, *popt), '--', color='red')
+    # plot histogram of fluctuating quantity in the center cell
+    # yhist, xhist, _ = ax[choice,0].hist(full_array[:,Nx // 2, Ny // 2], bins=25, color=col)
 
 plotVar = int(input("Show (0) or save (1) figure? "))
 
