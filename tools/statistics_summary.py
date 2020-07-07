43d9a849bef8313b40e262fea68246d3c48145ac
Hannes Holey <hannes.holey@kit.edu>
2020-07-07 14:10:07 +0200

    MAINT: process more than one file

--- tools/statistics_summary.py
+++ tools/statistics_summary.py
@@ -2,76 +2,81 @@
 
 import os
 import numpy as np
-from helper import getFile
+from helper import getFiles
 
-filename, file = getFile()
+# filename, file = getFile()
 
 toPlot = {0: ['jx', r'mass flux $x$ [kg/(m$^2$s)]'],
           1: ['jy', r'mass flux $y$ [kg/(m$^2$s)]'],
           2: ['rho', r'density [kg/m$^3$]'],
           3: ['p', r'pressure (MPa)']}
 
-Nx = file.Nx
-Ny = file.Ny
-dx = float(file.dx)
-dy = float(file.dy)
-dz = float(file.h1)
-dt = float(file.dt)
+files = getFiles()
 
-f_header_disc = f"{dx:.2g} {dy:.2g} {dz:.2g} {dt:.2g}"
-f_var_cols = "time, var(jx), var(jy), var(rho)"
-f_ac_cols = "time, ac_l(jx) ac_t(jx) ac_l(jy) ac_t(jy) ac(rho) ac(rho)"
+for f in files.values():
 
-time = np.array(file.variables["time"])
-out_var = np.empty([10, 4])
-out_ac = np.empty([len(time), 7])
+    filename = f[0]
+    file = f[1]
 
-wave_num = {0: [1, 0], 1: [0, 1]}
+    Nx = file.Nx
+    Ny = file.Ny
+    dx = float(file.dx)
+    dy = float(file.dy)
+    dz = float(file.h1)
+    dt = float(file.dt)
 
-kx = 2 * np.pi * np.fft.fftfreq(Nx, d=dx)
-ky = 2 * np.pi * np.fft.fftfreq(Ny, d=dy)
-out_ac[:,0] = time
+    f_header_disc = f"{dx:.2g} {dy:.2g} {dz:.2g} {dt:.2g}"
+    f_var_cols = "time, var(jx), var(jy), var(rho)"
+    f_ac_cols = "time, ac_l(jx) ac_t(jx) ac_l(jy) ac_t(jy) ac(rho) ac(rho)"
 
-for choice in np.arange(3):
+    time = np.array(file.variables["time"])
+    out_var = np.empty([10, 4])
+    out_ac = np.empty([len(time), 7])
 
-    try:
-        full_array = np.array(file.variables[toPlot[choice][0]], copy=False)
-    except MemoryError:
-        print("Not enough memory, using single precision!")
-        full_array = np.array(file.variables[toPlot[choice][0]], copy=False).astype(np.float32)
+    wave_num = {0: [1, 0], 1: [0, 1]}
 
-    mask = np.all(np.isfinite(full_array), axis=(1,2))
-    full_array = full_array[mask]
-    size = len(time)
+    kx = 2 * np.pi * np.fft.fftfreq(Nx, d=dx)
+    ky = 2 * np.pi * np.fft.fftfreq(Ny, d=dy)
+    out_ac[:,0] = time
 
-    n = 10
-    for i in range(n):
-        samples = (i + 1) * size // n
+    for choice in np.arange(3):
 
-        cellVariance = np.var(full_array[0:samples], axis=0)
-        # mean = np.mean(full_array[0:samples + 1])
-        variance = np.mean(cellVariance)
+        try:
+            full_array = np.array(file.variables[toPlot[choice][0]], copy=False)
+        except MemoryError:
+            print("Not enough memory, using single precision!")
+            full_array = np.array(file.variables[toPlot[choice][0]], copy=False).astype(np.float32)
 
-        out_var[i, choice + 1] = variance
-        out_var[i, 0] = time[samples - 1]
-        # out_var[i,1] = mean
-        # out_var[i,2] = variance
+        mask = np.all(np.isfinite(full_array), axis=(1,2))
+        full_array = full_array[mask]
+        size = len(time)
 
-    field_fft = np.real(np.fft.fft2(full_array))
+        n = 10
+        for i in range(n):
+            samples = (i + 1) * size // n
 
-    for d in [0, 1]:
-        if choice == 1:
-            iky, ikx = wave_num[d]
-        else:
-            ikx, iky = wave_num[d]
+            cellVariance = np.var(full_array[0:samples], axis=0)
+            # mean = np.mean(full_array[0:samples + 1])
+            variance = np.mean(cellVariance)
 
-        C = np.correlate(field_fft[:, ikx, iky], field_fft[:, ikx, iky], mode="full") / field_fft[:, ikx, iky].size
-        C = C[C.size // 2:]
-        C /= C[0]
+            out_var[i, choice + 1] = variance
+            out_var[i, 0] = time[samples - 1]
+            # out_var[i,1] = mean
+            # out_var[i,2] = variance
 
-        out_ac[:, 2 * choice + d + 1] = C
+        field_fft = np.real(np.fft.fft2(full_array))
 
-np.savetxt(os.path.splitext(filename)[0] + "_stats_var.dat", out_var, header=f_header_disc + "\n" + f_var_cols)
-np.savetxt(os.path.splitext(filename)[0] + "_stats_ac.dat", out_ac, header=f_header_disc + "\n" + f_ac_cols)
+        for d in [0, 1]:
+            if choice == 1:
+                iky, ikx = wave_num[d]
+            else:
+                ikx, iky = wave_num[d]
 
-# out_var[:,0] = np.array(file.variables["time"])
+            C = np.correlate(field_fft[:, ikx, iky], field_fft[:, ikx, iky], mode="full") / field_fft[:, ikx, iky].size
+            C = C[C.size // 2:]
+            C /= C[0]
+
+            out_ac[:, 2 * choice + d + 1] = C
+
+    np.savetxt(os.path.splitext(filename)[0] + "_stats_var.dat", out_var, header=f_header_disc + "\n" + f_var_cols)
+    np.savetxt(os.path.splitext(filename)[0] + "_stats_ac.dat", out_ac, header=f_header_disc + "\n" + f_ac_cols)
