11113b93fafd15ab37c029982e14db44159a9d87
Hannes Holey <hannes.holey@kit.edu>
2020-07-20 16:44:34 +0200

    MAINT: add Nx Ny to file header

--- tools/print_ACF.py
+++ tools/print_ACF.py
@@ -15,9 +15,11 @@ for f in files.values():
     dx = float(file.dx)
     dy = float(file.dy)
     dz = float(file.h1)
+    Nx = float(file.Nx)
+    Ny = float(file.Ny)
     dt = float(file.dt)
 
-    f_header_disc = f"{dx:.2g} {dy:.2g} {dz:.2g} {dt:.2g}"
+    f_header_disc = f"{dx:.2g} {dy:.2g} {dz:.2g} {Nx:.2g} {Ny:.2g} {dt:.2g}"
     f_ac_cols = "time, ac_l(jx) ac_t(jx) ac_l(jy) ac_t(jy) ac(rho) ac(rho)"
 
     out_ac = getTimeACF(file)
