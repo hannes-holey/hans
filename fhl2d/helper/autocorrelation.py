8a9696bf2b8d8ed570ba61a7d9d5d52c39a46d51
Hannes Holey <hannes.holey@kit.edu>
2020-11-19 16:29:54 +0100

    ENH: autocorrelation functions for multidimensional arrays

--- fhl2d/helper/autocorrelation.py
+++ fhl2d/helper/autocorrelation.py
@@ -75,3 +75,37 @@ def compute_ACF(time_series):
     C /= var
 
     return C
+
+
+def acf_fft(f_tq):
+    """
+    Compute normalized time autocorrelation function of multidimensional time
+    series using FFT (Wiener-Khinchin theorem). Since time is generally non-
+    periodic, the array is zero padded and the result of the inverse FFT is
+    divided by the size of the overlap.
+
+    Parameters
+    ----------
+    f_tq : numpy.ndarray
+        array containing the multidimensional time series, 1st axis is time
+
+    Returns
+    ----------
+    C : numpy.ndarray
+        ACF function
+    """
+
+    n = f_tq.shape[0]
+    var = np.var(f_tq, axis=0)
+    f_tq -= np.mean(f_tq, axis=0)
+
+    # pad array with zeros
+    ext_size = 2 * n - 1
+    fsize = 2**np.ceil(np.log2(ext_size)).astype('int')
+
+    # do fft and ifft
+    f_wq = np.fft.fft(f_tq, fsize, axis=0)
+    C_tq = np.fft.ifft(f_wq * f_wq.conjugate(), axis=0)[:n] / (n - np.arange(n))[:, np.newaxis, np.newaxis]
+    C_tq /= var
+
+    return C_tq.real
