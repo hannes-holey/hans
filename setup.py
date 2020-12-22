9e5ea93745cf8b1a608bdb94ebd688df3258420e
Hannes Holey <hannes.holey@kit.edu>
2020-12-22 14:05:44 +0100

    MAINT: add setup.py for installation, remove redundant requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='pylub',
+      description='A 2D Fluid Mechanics Solver for Lubrication',
+      url='http://github.com/hannes-holey/pylub',
+      author='Hannes Holey',
+      author_email='hannes.holey@kit.edu',
+      license="MIT",
+      packages=find_packages(),
+      scripts=['cli/plot1D_evolution.py',
+               'cli/plot1D_last.py',
+               'cli/plot_scalar.py',
+               'cli/read_config.py',
+               'cli/conf_to_yml.py',
+               'cli/animate2d.py'],
+      test_suite='tests',
+      install_requires=["numpy>=1.18.1",
+                        "matplotlib>=3.2.0",
+                        "PyYAML>=1.18.1",
+                        "GitPython>=3.1.0",
+                        "netCDF4>=1.5.3"],
+      zip_safe=False)
