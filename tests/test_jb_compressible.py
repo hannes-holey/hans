34a18e42a112718cde3e1b7444f59a8ab4570dd8
Hannes Holey <hannes.holey@kit.edu>
2021-01-05 10:40:58 +0100

    TST: add mass conservation test, tear down with shutil

--- tests/test_jb_compressible.py
+++ tests/test_jb_compressible.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 import netCDF4
 import numpy as np
@@ -13,7 +14,8 @@ class TestCompressibleJournalBearing(unittest.TestCase):
     def setUpClass(cls):
         config_file = os.path.join("examples", "journal-bearing.yaml")
         cls.tmp_dir = os.path.join("tests", "tmp")
-        os.makedirs(cls.tmp_dir)
+        if not os.path.exists(cls.tmp_dir):
+            os.makedirs(cls.tmp_dir)
 
         myTestProblem = Input(config_file).getProblem()
         myTestProblem.disc["Ny"] = 3
@@ -22,14 +24,13 @@ class TestCompressibleJournalBearing(unittest.TestCase):
 
         ds = netCDF4.Dataset(os.path.join(cls.tmp_dir, "journal-bearing_0001.nc"))
         cls.rho = ds.variables["rho"]
+        cls.mass = ds.variables["mass"]
 
     @classmethod
     def tearDownClass(cls):
-        os.remove(os.path.join(cls.tmp_dir, "journal-bearing_0001.nc"))
-        os.removedirs(cls.tmp_dir)
+        shutil.rmtree(cls.tmp_dir, ignore_errors=True)
 
     def test_pressure(self):
-
         p = EquationOfState(self.material).isoT_pressure(np.array(self.rho[-1]))
         p = p[:, p.shape[1] // 2] / 1e6
         p_ref = np.loadtxt(os.path.join("tests", "rey_comp_journal_0.7.dat"), usecols=(1,)) / 1e6
@@ -37,12 +38,16 @@ class TestCompressibleJournalBearing(unittest.TestCase):
         np.testing.assert_almost_equal(p, p_ref, decimal=1)
 
     def test_density(self):
-
         rho = np.array(self.rho[-1])
         rho = rho[:, rho.shape[1] // 2]
         rho_ref = np.loadtxt(os.path.join("tests", "rey_comp_journal_0.7.dat"), usecols=(0,))
 
         np.testing.assert_almost_equal(rho, rho_ref, decimal=1)
+
+    def test_massConservation(self):
+        mass = np.array(self.mass)
+        relDiff = abs(mass[-1] - mass[0]) / mass[0]
+        self.assertLess(relDiff, 0.001)
 
 
 if __name__ == "__main__":
--- tests/test_jb_incompressible.py
+++ tests/test_jb_incompressible.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 import netCDF4
 import numpy as np
@@ -9,31 +10,37 @@ from pylub.eos import EquationOfState
 
 class TestIncompressibleJournalBearing(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         config_file = os.path.join("examples", "journal-bearing_incomp.yaml")
-        self.tmp_dir = os.path.join("tests", "tmp")
-        os.makedirs(self.tmp_dir)
+        cls.tmp_dir = os.path.join("tests", "tmp")
+        if not os.path.exists(cls.tmp_dir):
+            os.makedirs(cls.tmp_dir)
 
         myTestProblem = Input(config_file).getProblem()
         myTestProblem.disc["Ny"] = 3
-        self.material = myTestProblem.material
+        cls.material = myTestProblem.material
+        myTestProblem.run(out_dir=cls.tmp_dir)
 
-        myTestProblem.run(out_dir=self.tmp_dir)
+        ds = netCDF4.Dataset(os.path.join(cls.tmp_dir, "journal-bearing_incomp_0001.nc"))
+        cls.rho = ds.variables["rho"]
+        cls.mass = ds.variables["mass"]
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.tmp_dir, ignore_errors=True)
 
     def test_pressure(self):
-        ds = netCDF4.Dataset(os.path.join(self.tmp_dir, "journal-bearing_incomp_0001.nc"))
-        rho = np.array(ds.variables["rho"])[-1]
-
-        p = EquationOfState(self.material).isoT_pressure(rho)
+        p = EquationOfState(self.material).isoT_pressure(np.array(self.rho)[-1])
         p = p[:, p.shape[1] // 2] / 1e6
-
         p_ref = np.loadtxt(os.path.join("tests", "rey_incomp_journal_0.7.dat"), usecols=(1,)) / 1e6
 
         np.testing.assert_almost_equal(p, p_ref, decimal=1)
 
-    def tearDown(self):
-        os.remove(os.path.join(self.tmp_dir, "journal-bearing_incomp_0001.nc"))
-        os.removedirs(self.tmp_dir)
+    def test_massConservation(self):
+        mass = np.array(self.mass)
+        relDiff = abs(mass[-1] - mass[0]) / mass[0]
+        self.assertLess(relDiff, 0.001)
 
 
 if __name__ == "__main__":
