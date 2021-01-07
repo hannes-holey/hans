c76524d388638d93abf02d497c8073a01f1ba169
Hannes Holey <hannes.holey@kit.edu>
2021-01-07 11:19:40 +0100

    BUG: resolve import error

--- cli/plot_scalar.py
+++ cli/plot_scalar.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-
-from pylub.helper.data_parser import getData
-from pylub.helper.plot import plot_timeseries
+from pylub.helper.plot import Plot
 
 if __name__ == "__main__":
 
     # TODO: ArgumentParser
-    files = getData(".")
-    fig, ax = plot_timeseries(files, "dt")
+    files = Plot(".")
+    fig, ax = files.plot_timeseries("mass")
     plt.show()
