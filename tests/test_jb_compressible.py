e99c89a32b0246e9a4e75cccfbfb60e87bb464ca
Hannes Holey <hannes.holey@kit.edu>
2021-01-13 10:19:07 +0100

    TST: modify journal bearing tests to comply with new BC implementation

--- tests/test_jb_compressible.py
+++ tests/test_jb_compressible.py
@@ -12,19 +12,20 @@ class TestCompressibleJournalBearing(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        config_file = os.path.join("examples", "journal-bearing.yaml")
+        config_file = os.path.join("examples", "jb_inf_DH.yaml")
         cls.tmp_dir = os.path.join("tests", "tmp")
         if not os.path.exists(cls.tmp_dir):
             os.makedirs(cls.tmp_dir)
 
         myTestProblem = Input(config_file).getProblem()
-        myTestProblem.disc["Ny"] = 3
         cls.material = myTestProblem.material
         myTestProblem.run(out_dir=cls.tmp_dir)
 
-        ds = netCDF4.Dataset(os.path.join(cls.tmp_dir, "journal-bearing_0001.nc"))
+        ds = netCDF4.Dataset(os.path.join(cls.tmp_dir, "jb_inf_DH_0001.nc"))
         cls.rho = ds.variables["rho"]
         cls.mass = ds.variables["mass"]
+
+        cls.rho_ref, cls.p_ref = np.loadtxt(os.path.join("tests", "jb_0.7_inf_DH_ref.dat"), unpack=True)
 
     @classmethod
     def tearDownClass(cls):
@@ -33,16 +34,15 @@ class TestCompressibleJournalBearing(unittest.TestCase):
     def test_pressure(self):
         p = EquationOfState(self.material).isoT_pressure(np.array(self.rho[-1]))
         p = p[:, p.shape[1] // 2] / 1e6
-        p_ref = np.loadtxt(os.path.join("tests", "rey_comp_journal_0.7.dat"), usecols=(1,)) / 1e6
+        self.p_ref /= 1e6
 
-        np.testing.assert_almost_equal(p, p_ref, decimal=1)
+        np.testing.assert_almost_equal(p, self.p_ref, decimal=1)
 
     def test_density(self):
         rho = np.array(self.rho[-1])
         rho = rho[:, rho.shape[1] // 2]
-        rho_ref = np.loadtxt(os.path.join("tests", "rey_comp_journal_0.7.dat"), usecols=(0,))
 
-        np.testing.assert_almost_equal(rho, rho_ref, decimal=1)
+        np.testing.assert_almost_equal(rho, self.rho_ref, decimal=1)
 
     def test_massConservation(self):
         mass = np.array(self.mass)
