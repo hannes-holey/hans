7fd3b5ba8e19d99f3bb07204f25f60543b913e69
Hannes Holey <hannes.holey@kit.edu>
2021-03-16 10:07:51 +0100

    MAINT: rm redundant type casting, stress notation w/ zeta and eta

--- pylub/stress.py
+++ pylub/stress.py
@@ -14,15 +14,17 @@ class SymStressField2D(VectorField):
 
     def set(self, q, h):
 
-        U = float(self.geo['U'])
-        V = float(self.geo['V'])
+        U = self.geo['U']
+        V = self.geo['V']
         eta = EquationOfState(self.mat).viscosity(U, V, q[0], h[0])
-        zeta = float(self.mat['bulk'])
-        lam = zeta - 2 / 3 * eta
+        zeta = self.mat['bulk']
+
+        visc_1 = zeta + 4 / 3 * eta
+        visc_2 = zeta - 2 / 3 * eta
 
         # origin bottom, U_top = 0, U_bottom = U
-        self._field[0] = -((U * q[0] - 3 * q[1]) * (lam + 2 * eta) * h[1] + (V * q[0] - 3 * q[2]) * lam * h[2]) / (h[0] * q[0])
-        self._field[1] = -((V * q[0] - 3 * q[2]) * (lam + 2 * eta) * h[2] + (U * q[0] - 3 * q[1]) * lam * h[1]) / (h[0] * q[0])
+        self._field[0] = -3 * ((U * q[0] - q[1]) * visc_1 * h[1] + (V * q[0] - q[2]) * visc_2 * h[2]) / (h[0] * q[0])
+        self._field[1] = -3 * ((V * q[0] - q[2]) * visc_1 * h[2] + (U * q[0] - q[1]) * visc_2 * h[1]) / (h[0] * q[0])
         self._field[2] = -eta * ((V * q[0] - 3 * q[2]) * h[1] + (U * q[0] - 3 * q[1]) * h[2]) / (h[0] * q[0])
 
 
@@ -38,18 +40,20 @@ class SymStressField3D(TensorField):
 
     def set(self, q, h, bound):
 
-        U = float(self.geo['U'])
-        V = float(self.geo['V'])
+        U = self.geo['U']
+        V = self.geo['V']
         eta = EquationOfState(self.mat).viscosity(U, V, q[0], h[0])
-        zeta = float(self.mat['bulk'])
-        lam = zeta - 2 / 3 * eta
+        zeta = self.mat['bulk']
+
+        visc_1 = zeta + 4 / 3 * eta
+        visc_2 = zeta - 2 / 3 * eta
 
         if bound == "top":
 
             # origin bottom, U_top = 0, U_bottom = U
-            self._field[0] = (-2 * (U * q[0] - 3 * q[1]) * (2 * eta + lam) * h[1] - 2 * (V * q[0] - 3 * q[2]) * lam * h[2]) / (h[0] * q[0])
-            self._field[1] = (-2 * (V * q[0] - 3 * q[2]) * (2 * eta + lam) * h[2] - 2 * (U * q[0] - 3 * q[1]) * lam * h[1]) / (h[0] * q[0])
-            self._field[2] = -2 * lam * ((U * q[0] - 3 * q[1]) * h[1] + (V * q[0] - 3 * q[2]) * h[2]) / (q[0] * h[0])
+            self._field[0] = -2 * ((U * q[0] - 3 * q[1]) * visc_1 * h[1] + (V * q[0] - 3 * q[2]) * visc_2 * h[2]) / (h[0] * q[0])
+            self._field[1] = -2 * ((V * q[0] - 3 * q[2]) * visc_1 * h[2] + (U * q[0] - 3 * q[1]) * visc_2 * h[1]) / (h[0] * q[0])
+            self._field[2] = -2 * visc_2 * ((U * q[0] - 3 * q[1]) * h[1] + (V * q[0] - 3 * q[2]) * h[2]) / (h[0] * q[0])
             self._field[3] = 2 * eta * (V * q[0] - 3 * q[2]) / (q[0] * h[0])
             self._field[4] = 2 * eta * (U * q[0] - 3 * q[1]) / (q[0] * h[0])
             self._field[5] = -2 * eta * ((V * q[0] - 3 * q[2]) * h[1] + h[2] * (U * q[0] - 3 * q[1])) / (q[0] * h[0])
