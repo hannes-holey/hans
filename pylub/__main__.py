0c7d09ddb3975ef31c78d4d63aaf2a0e09989164
Hannes Holey <hannes.holey@kit.edu>
2021-04-06 17:35:37 +0200

    MAINT: live plotting only available in serial

--- pylub/__main__.py
+++ pylub/__main__.py
@@ -25,6 +25,10 @@ if __name__ == "__main__":
         parser = get_parser()
         args = parser.parse_args()
 
+        if args.plot and comm.Get_size() > 1:
+            print("Live-plotting not implemented for parallel execution", flush=True)
+            comm.Abort()
+
         inputFile = os.path.join(os.getcwd(), args.filename)
         if args.restart_file is not None:
             restartFile = os.path.join(os.getcwd(), args.restart_file)
