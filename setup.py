68fe9682b8de8f3439eb995e9a6084a9dd6a1a3c
Hannes Holey <hannes.holey@kit.edu>
2021-05-19 14:47:28 +0200

    MAINT: change install requirements, add license header

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
+"""
+MIT License
+
+Copyright 2021 Hannes Holey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
 from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as fh:
@@ -19,10 +44,9 @@ setup(name='hans',
                'cli/read_config.py',
                'cli/animate2d.py'],
       test_suite='tests',
-      install_requires=["numpy>=1.18.1",
-                        "matplotlib>=3.2.0",
-                        "PyYAML>=5.3",
-                        "GitPython>=3.1.0",
-                        "netCDF4>=1.5.3",
-                        "pytest>4"],
+      tests_require=["pytest>=4"],
+      install_requires=requirements,
+      python_requires=">=3.6",
+      use_scm_version=True,
+      setup_requires=['setuptools_scm>=3.5.0'],
       zip_safe=False)
