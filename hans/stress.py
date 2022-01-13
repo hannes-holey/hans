d64e578054882454255d2c685274e6d4fa16d46e
Hannes Holey <hannes.holey@kit.edu>
2022-01-13 10:13:15 +0100

    DOC: update copyright string

--- hans/stress.py
+++ hans/stress.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright 2021 Hannes Holey
+Copyright 2021, 2022 Hannes Holey
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
