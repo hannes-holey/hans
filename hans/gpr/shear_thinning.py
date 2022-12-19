db4e388b26fbc1876b5b90a254986708edcf34a5
Hannes Holey <hannes.holey@kit.edu>
2022-12-19 16:42:28 +0100

    WIP: heteroscedastic GP

--- hans/gpr/shear_thinning.py
+++ hans/gpr/shear_thinning.py
@@ -29,20 +29,28 @@ from scipy.interpolate import interp1d
 def GPRegression(file):
 
     # NOTE: training in 1/nanoseconds - mPas
-    srate, visc, visc_err = np.loadtxt(file, delimiter=",", unpack=True)
+
+    # TODO: should only depend on input file
+    # glycosylated
+    # srate, visc, visc_err, _, _ = np.loadtxt(file, delimiter=",", unpack=True)
+
+    # non-glycosylated
+    srate, _, _, visc, visc_err = np.loadtxt(file, delimiter=",", unpack=True)
 
     # TODO: other input: homo/hetero, (un)fixed noise, kernel, prior, output (mean or sample) ...
-
     xtrain = srate[:, None]
     ytrain = visc[:, None]
+    yerr = visc_err[:, None]**2
 
-    # TODO: do not recompute GP every time viscosity is called (stoer m somewhere)
+    # kernel
     k = GPy.kern.RBF(1)
+
+    # homoscedastic
     m = GPy.models.GPRegression(xtrain, ytrain, k)
 
     # heteroscedastic
-    # m = GPy.models.GPHeteroscedasticRegression(srate, visc, k)
-    # m['.*het_Gauss.variance'] = visc_err    # set the noise to noise of training data
+    # m = GPy.models.GPHeteroscedasticRegression(xtrain, ytrain, k)
+    # m['.*het_Gauss.variance'] = yerr     # set the noise to noise of training data
     # m.het_Gauss.variance.fix()              # fix noise parameter before optimization
 
     m.optimize('bfgs', max_iters=100)
@@ -54,17 +62,17 @@ def interpolate(shear_rate, material):
     m = material["model"]
 
     # rescale shear rate
-    # shear_rate /= 1e9
+    shear_rate /= 1e9
 
     nx, ny = shear_rate.shape
 
     shear_rate = shear_rate.flatten()[:, None]
 
-    mean, cov = m.predict(shear_rate, full_cov=True)
+    mean, cov = m.predict(shear_rate, full_cov=True, include_likelihood=True)
 
     # rescale posterior mean and covariance
-    # mean *= 1e-3
-    # cov *= 1e-6
+    mean *= 1e-3
+    cov *= 1e-6
 
     if material["sampling"] == "mean":
         out = mean[:, 0]
@@ -75,9 +83,9 @@ def interpolate(shear_rate, material):
         # new in version 1.18.0
         # methods to compute factor matrix: svd, eigh, cholesky (increasing speed)
         rng = np.random.default_rng()
-        out = rng.multivariate_normal(mean[:, 0], cov, method="cholesky")
+        out = rng.multivariate_normal(mean[:, 0], cov, method="svd")
 
     # rescale
-    # return interp1d(shear_rate[:, 0] * 1e9, out)
+    return interp1d(shear_rate[:, 0] * 1e9, out)
 
-    return interp1d(shear_rate[:, 0], out)
+    # return interp1d(shear_rate[:, 0], out)
