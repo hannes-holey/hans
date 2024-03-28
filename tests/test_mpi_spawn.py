ab2673368063f9220894a289f17d00e122679d3f
Hannes Holey <hannes.holey@kit.edu>
2024-03-28 10:04:47 +0100

    TST: test range of MPI processes

--- tests/test_mpi_spawn.py
+++ tests/test_mpi_spawn.py
@@ -1,11 +1,19 @@
 import os
+import sys
 import subprocess
+import pytest
 
 
-def test_spawn():
+@pytest.fixture(scope="session", params=[1, 2, 3, 4, 5, 6, 7, 8])
+def command(request):
 
     fname = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'spawner.py')
-    command = f'mpirun -n 1 --oversubscribe python {fname} 3'
+    command = f'mpirun -n 1 --oversubscribe {sys.executable} {fname} {request.param}'
+
+    yield command
+
+
+def test_spawn(command):
 
     try:
         subprocess.run(command.split(), check=True, stderr=True, env=os.environ)
