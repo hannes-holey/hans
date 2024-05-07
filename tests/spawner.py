d05ad71ee9b3209b4a5b3408fb563fa7e7f345c8
Hannes Holey <hannes.holey@kit.edu>
2024-05-07 22:43:08 +0200

    TST: adapt spawning test. Synchronize parent and child processes

--- tests/spawner.py
+++ tests/spawner.py
@@ -6,40 +6,46 @@ from mpi4py import MPI
 def main():
 
     WORKER_COMMAND = 'worker'
-    SHOULD_MERGE = True
-    SHOULD_DISCONNECT = False
 
     command = len(sys.argv) > 1 and sys.argv[1] or '1'
     if command != WORKER_COMMAND:
         worker_count = int(command)
-        print('launching {} workers.'.format(worker_count))
-        comm = MPI.COMM_SELF.Spawn(sys.executable,
-                                   args=[sys.argv[0], WORKER_COMMAND],
-                                   maxprocs=worker_count)
-        print('launched workers.')
-        if SHOULD_MERGE:
-            comm = comm.Merge()
-            print("Merged workers.")
+        print('Manager: launching {} worker.'.format(worker_count))
+        sub_comm = MPI.COMM_SELF.Spawn(sys.executable,
+                                       args=[sys.argv[0], WORKER_COMMAND],
+                                       maxprocs=worker_count)
+
+        common_comm = sub_comm.Merge()
+        common_comm.Barrier()
+        print("Manager: merged workers.")
+
         for i in range(worker_count):
-            msg = comm.recv(source=MPI.ANY_SOURCE)
-            print("Manager received {}.".format(msg))
-        print("Manager finished with fleet size {}.".format(comm.Get_size()))
+            msg = common_comm.recv(source=MPI.ANY_SOURCE)
+            print(f"Manager: received {msg}.")
+
+        print(f"Manager: finished with fleet size {common_comm.Get_size()}.")
+
+        common_comm.Barrier()
+        common_comm.Free()
+
     else:
-        print('worker launched.')
         comm = MPI.Comm.Get_parent()
-        print("Got parent.")
-        if SHOULD_MERGE:
-            comm = comm.Merge()
-            print("Merged parent.")
         size = comm.Get_size()
         rank = comm.Get_rank()
+        print(f'Worker {rank}/{size}: launched.')
+        print(f"Worker {rank}/{size}: got parent.")
+
+        comm = comm.Merge()
+        print(f"Worker {rank}/{size}: merged parent.")
+
+        comm.Barrier()
+
         comm.send(rank, dest=0)
 
-        print("Finished worker: rank {} of {}".format(rank, size))
+        print(f"Worker {rank}/{size}: finished")
 
-    if SHOULD_DISCONNECT:
-        comm.Disconnect()
-        print("Finished with command {}.".format(command))
+        comm.Barrier()
+        comm.Free()
 
 
 if __name__ == '__main__':
