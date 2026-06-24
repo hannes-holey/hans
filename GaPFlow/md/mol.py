BUG: fix sign wall rotation

GaPFlow/md/mol.py
@@ -88,7 +88,7 @@ class GoldAlkane(MolecularDynamics):
 
         if self.params['wall_rotation']:
             dh_dx = float(X[4])
-            args["rotation"] = -np.arctan(dh_dx) / np.pi * 180.
+            args["rotation"] = np.arctan(dh_dx) / np.pi * 180.
 
         cwd = os.getcwd()
         os.chdir(proto_ds_datapath)